a5bbce3292ae33ffaac74693cb7f5696781de438
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2025-03-10 21:37:37 +0700

    update

--- src/overtime_record/overtime_record.module.ts
+++ src/overtime_record/overtime_record.module.ts
@@ -3,6 +3,7 @@ import { OvertimeRecordController } from './overtime_record.controller';
 import { OvertimeRecordService } from './overtime_record.service';
 import { OvertimeRecord, OverTimeRecordSchema } from './overtime_record.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [OvertimeRecordController],
@@ -14,6 +15,11 @@ import { MongooseModule } from '@nestjs/mongoose';
         schema: OverTimeRecordSchema,
       },
     ]),
+    JwtModule.register({
+      secret: 'yourSecretKey',
+
+      signOptions: { expiresIn: '60m' },
+    }),
   ],
 })
 export class OvertimeRecordModule {}
