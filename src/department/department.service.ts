c2e2a210c4349da376b0edb0131ecfb7f1f44f09
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2025-03-20 10:21:17 +0700

    update

--- src/department/department.service.ts
+++ src/department/department.service.ts
@@ -115,7 +115,7 @@ export class DepartmentService {
         },
       },
       {
-        $unwind: '$managerDetails',
+        $unwind: { path: '$managerDetails', preserveNullAndEmptyArrays: true },
       },
       {
         $addFields: {
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -31,7 +31,10 @@ export class EmployeeService {
         },
       },
       {
-        $unwind: '$departmentDetails',
+        $unwind: {
+          path: '$departmentDetails',
+          preserveNullAndEmptyArrays: true,
+        },
       },
       {
         $addFields: {
