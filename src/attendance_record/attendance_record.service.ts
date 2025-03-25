292ef094c194d4acd657535a04fded804d611584
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2025-03-25 09:55:46 +0700

    update

--- src/attendance_record/attendance_record.service.ts
+++ src/attendance_record/attendance_record.service.ts
@@ -213,7 +213,6 @@ export class AttendanceRecordService {
         },
       },
       {
-        preserveNullAndEmptyArrays: true,
         $unwind: { path: '$employeeDetails' },
       },
       {
